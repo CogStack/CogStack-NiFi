3df68a3e407480672a4dabc23381712781e4d755
lrog <lucas.roguski@gmail.com>
2019-12-09 16:18:00 +0000

    using avro schema definitions directly stored (at the moment) as external files

--- nifi/devel/user-scripts/parse-docs-from-es-to-avro.groovy
+++ nifi/devel/user-scripts/parse-docs-from-es-to-avro.groovy
@@ -13,93 +13,87 @@ import org.apache.nifi.processor.io.StreamCallback
 import java.nio.charset.StandardCharsets
 
 
-// Avro schema used in the database
-// and as provided by Tika Service
+// avro schema instance
 //
-def avroDocumentSchema = 
-'''
-{
-  "type": "record",
-  "name": "document",
-  "fields":
-  [
-    { "name": "doc_id", "type": "int" },
-    { "name": "doc_text", "type": "string", "default": "" },
-    { "name": "processing_timestamp", "type": { "type" : "string", "logicalType" : "timestamp-millis" } },
-    { "name": "metadata_x_ocr_applied", "type": "boolean" },
-    { "name": "metadata_x_parsed_by", "type": "string" },
-    { "name": "metadata_content_type", "type": ["null", "string"], "default": null },
-    { "name": "metadata_page_count", "type": ["null", "int"], "default": null },
-    { "name": "metadata_creation_date", "type": ["null", { "type" : "string", "logicalType" : "timestamp-millis" }], "default": null },
-    { "name": "metadata_last_modified", "type": ["null", { "type" : "string", "logicalType" : "timestamp-millis" }], "default": null }
-  ]
+class SchemaInstance {
+	static Schema avroSchema = null
+
+	static loadSchema(text) {
+		avroSchema = new Schema.Parser().parse(text)
+	}
 }
-'''
+
+
+// loads the schema when processor starts
+//
+static onStart(ProcessContext context) {
+	def filePath = context.getProperty('avro_schema_path') as String
+	File file = new File(filePath)
+	SchemaInstance.loadSchema(file.text)
+}
+
+
+// helper functions
+//
+def parseJsonToAvro(hit, avroSchema) {
+	// Create a new, generic record
+	GenericRecord docRecord = new GenericData.Record(avroSchema)
+
+	// parse the json
+	//
+	assert hit['doc_id']
+    docRecord.put("doc_id", hit['doc_id'])
+
+    assert hit['timestamp']
+    docRecord.put("processing_timestamp", hit['timestamp'])
+
+    if (hit['text'])
+    	docRecord.put("doc_text", new org.apache.avro.util.Utf8(hit['text']))
+
+    // these metadata fields will be always provided by tika
+    //
+    if (hit.metadata['X-OCR-Applied'])
+    	docRecord.put("metadata_x_ocr_applied", Boolean.parseBoolean(hit.metadata['X-OCR-Applied']))
+
+    if (hit.metadata['X-Parsed-By'])
+    	docRecord.put("metadata_x_parsed_by", new org.apache.avro.util.Utf8(String.join(";", hit.metadata['X-Parsed-By'])))
+
+
+    // optional metadata fields
+    //
+    if (hit.metadata['Content-Type'])
+    	docRecord.put("metadata_content_type", new org.apache.avro.util.Utf8(hit.metadata['Content-Type']))
+
+    if (hit.metadata['Creation-Date'])
+    	docRecord.put("metadata_creation_date", hit.metadata['Creation-Date'])
+
+    if (hit.metadata['Last-Modified'])
+    	docRecord.put("metadata_last_modified", hit.metadata['Last-Modified'])
+
+    return docRecord
+}
+
+
+
 
 def flowFile = session.get();
 if (flowFile == null) {
     return;
 }
 
-//try {
 
+try {
     flowFile = session.write(flowFile, {inStream, outStream ->
 
         def flowFileContent = IOUtils.toString(inStream, StandardCharsets.UTF_8)
         def inJson = new JsonSlurper().parseText(flowFileContent)
 
-        // Defining avro reader and writer
+        // Defining a generic avro reader and writer
         DataFileWriter<GenericRecord> writer = new DataFileWriter<>(new GenericDatumWriter<GenericRecord>())
-
-        // set avro schema
-        def docSchema = new Schema.Parser().parse(avroDocumentSchema)
-
-
-        // Define wich schema to be used for writing
-        // If you want to extend or change the output record format
-        // you define a new schema and specify that it shall be used for writing
-        writer.create(docSchema, outStream)
-
+        writer.create(SchemaInstance.avroSchema, outStream)
 
         inJson.hits.each { hit ->
-
-            // Create a new record
-            GenericRecord docRecord = new GenericData.Record(docSchema)
-
-            // parse the result and populate the record with data
-
-            // obligatory fields
-            //
-            assert hit['doc_id']
-            docRecord.put("doc_id", hit['doc_id'])
-
-            assert hit['timestamp']
-            docRecord.put("processing_timestamp", hit['timestamp'])
-
-            if (hit['text'])
-            	docRecord.put("doc_text", new org.apache.avro.util.Utf8(hit['text']))
-
-            // these metadata fields will be always provided by tika
-            //
-            if (hit.metadata['X-OCR-Applied'])
-            	docRecord.put("metadata_x_ocr_applied", Boolean.parseBoolean(hit.metadata['X-OCR-Applied']))
-
-            if (hit.metadata['X-Parsed-By'])
-            	docRecord.put("metadata_x_parsed_by", new org.apache.avro.util.Utf8(String.join(";", hit.metadata['X-Parsed-By'])))
-
-
-            // optional metadata fields
-            //
-            if (hit.metadata['Content-Type'])
-            	docRecord.put("metadata_content_type", new org.apache.avro.util.Utf8(hit.metadata['Content-Type']))
-
-            if (hit.metadata['Creation-Date'])
-            	docRecord.put("metadata_creation_date", hit.metadata['Creation-Date'])
-
-            if (hit.metadata['Last-Modified'])
-            	docRecord.put("metadata_last_modified", hit.metadata['Last-Modified'])
-
-            // Append a new record to avro file
+            def docRecord = parseJsonToAvro(hit, SchemaInstance.avroSchema)
             writer.append(docRecord)
         }
 
@@ -107,13 +101,10 @@ if (flowFile == null) {
         writer.close()
 
     } as StreamCallback)
-
     session.transfer(flowFile, REL_SUCCESS)
-
-/*
-} catch(e) {
-    log.error('Error appending new record to avro file', e)
+} 
+catch(e) {
+    log.error('Error while parsing to avro record', e)
     flowFile = session.penalize(flowFile)
     session.transfer(flowFile, REL_FAILURE)
 }
-*/
--- nifi/devel/user-scripts/parse-tika-result-json-to-avro.groovy
+++ nifi/devel/user-scripts/parse-tika-result-json-to-avro.groovy
@@ -13,6 +13,26 @@ import org.apache.nifi.processor.io.StreamCallback
 import java.nio.charset.StandardCharsets
 
 
+// avro schema instance
+//
+class SchemaInstance {
+    static Schema avroSchema = null
+
+    static loadSchema(text) {
+        avroSchema = new Schema.Parser().parse(text)
+    }
+}
+
+
+// loads the schema when processor starts
+//
+static onStart(ProcessContext context) { 
+    def filePath = context.getProperty('avro_schema_path') as String
+    File file = new File(filePath)
+    SchemaInstance.loadSchema(file.text)
+}
+
+
 // get the input flow file
 def flowFile = session.get();
 if (flowFile == null) {
@@ -20,53 +40,11 @@ if (flowFile == null) {
 }
 
 
-// Avro schema used in the database
-// and as provided by Tika Service
+// helper functions
 //
-def avroDocumentSchema = 
-'''
-{
-  "type": "record",
-  "name": "document",
-  "fields":
-  [
-    { "name": "doc_id", "type": ["int", "string"] },
-    { "name": "doc_text", "type": "string", "default": "" },
-    { "name": "processing_timestamp", "type": { "type" : "string", "logicalType" : "timestamp-millis" } },
-    { "name": "metadata_x_ocr_applied", "type": "boolean" },
-    { "name": "metadata_x_parsed_by", "type": "string" },
-    { "name": "metadata_content_type", "type": ["null", "string"], "default": null },
-    { "name": "metadata_page_count", "type": ["null", "int"], "default": null },
-    { "name": "metadata_creation_date", "type": ["null", { "type" : "string", "logicalType" : "timestamp-millis" }], "default": null },
-    { "name": "metadata_last_modified", "type": ["null", { "type" : "string", "logicalType" : "timestamp-millis" }], "default": null }
-  ]
-}
-'''
-
-
-// Parse the input json
-//
-flowFile = session.write(flowFile, { inputStream, outputStream ->
-
-	def content = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-	def inJson = new JsonSlurper().parseText(content)
-
-	// Defining avro reader and writer
-	DataFileWriter<GenericRecord> writer = new DataFileWriter<>(new GenericDatumWriter<GenericRecord>())
-
-	// set avro schema
-	def docSchema = new Schema.Parser().parse(avroDocumentSchema)
-
-
-    // Define wich schema to be used for writing
-    // If you want to extend or change the output record format
-    // you define a new schema and specify that it shall be used for writing
-    writer.create(docSchema, outputStream)
-
-    // Create a new record
-    GenericRecord docRecord = new GenericData.Record(docSchema)
-
-    // parse the result and populate the record with data
+def parseJsonToAvro(inJson, avroSchema) {
+    // create a new avro record
+     GenericRecord docRecord = new GenericData.Record(avroSchema)
 
     // obligatory fields
     //
@@ -84,17 +62,17 @@ flowFile = session.write(flowFile, { inputStream, outputStream ->
     docRecord.put("processing_timestamp", inJson['timestamp'])
 
     if (inJson.containsKey('text'))
-    	docRecord.put("doc_text", new org.apache.avro.util.Utf8(inJson['text']))
+        docRecord.put("doc_text", new org.apache.avro.util.Utf8(inJson['text']))
 
 
     // these metadata fields will be always provided by tika
     //
     assert inJson.containsKey('metadata')
     if (inJson.metadata.containsKey('X-OCR-Applied'))
-    	docRecord.put("metadata_x_ocr_applied", inJson.metadata['X-OCR-Applied'])
+        docRecord.put("metadata_x_ocr_applied", inJson.metadata['X-OCR-Applied'])
 
     if (inJson.metadata.containsKey('X-Parsed-By'))
-    	docRecord.put("metadata_x_parsed_by", new org.apache.avro.util.Utf8(String.join(";", inJson.metadata['X-Parsed-By'])))
+        docRecord.put("metadata_x_parsed_by", new org.apache.avro.util.Utf8(String.join(";", inJson.metadata['X-Parsed-By'])))
 
 
     // optional metadata fields
@@ -103,20 +81,44 @@ flowFile = session.write(flowFile, { inputStream, outputStream ->
         docRecord.put("metadata_page_count", inJson.metadata['Page-Count'])
 
     if (inJson.metadata.containsKey('Content-Type'))
-    	docRecord.put("metadata_content_type", new org.apache.avro.util.Utf8(inJson.metadata['Content-Type']))
+        docRecord.put("metadata_content_type", new org.apache.avro.util.Utf8(inJson.metadata['Content-Type']))
 
     if (inJson.metadata.containsKey('Creation-Date'))
-    	docRecord.put("metadata_creation_date", inJson.metadata['Creation-Date'])
+        docRecord.put("metadata_creation_date", inJson.metadata['Creation-Date'])
 
     if (inJson.metadata.containsKey('Last-Modified'))
-    	docRecord.put("metadata_last_modified", inJson.metadata['Last-Modified'])
+        docRecord.put("metadata_last_modified", inJson.metadata['Last-Modified'])
 
-    // Append a new record to avro file
-    writer.append(docRecord)
-    
-    // do not forget to close the writer
-    writer.close()
-} as StreamCallback)
+    return docRecord
+}
 
-// transfer the seesions file
-session.transfer(flowFile, REL_SUCCESS)
+
+// process the flow file
+//
+//try {
+    flowFile = session.write(flowFile, { inputStream, outputStream ->
+
+    	def content = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+    	def inJson = new JsonSlurper().parseText(content)
+
+    	// Defining avro writer
+    	DataFileWriter<GenericRecord> writer = new DataFileWriter<>(new GenericDatumWriter<GenericRecord>())
+        writer.create(SchemaInstance.avroSchema, outputStream)
+
+        docRecord = parseJsonToAvro(inJson, SchemaInstance.avroSchema)
+        writer.append(docRecord)
+        
+        // do not forget to close the writer
+        writer.close()
+    } as StreamCallback)
+
+    // transfer the seesions file
+    session.transfer(flowFile, REL_SUCCESS)
+//} 
+//catch(e) {
+    /*
+    log.error('Error while parsing to avro record', e)
+    flowFile = session.penalize(flowFile)
+    session.transfer(flowFile, REL_FAILURE)
+    */
+//}
