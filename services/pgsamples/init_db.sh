2666da7b1084d08f0a759be2146031b4fa6f088d
vladd-bit <vlad.a.dinu@gmail.com>
2021-10-18 10:22:51 +0100

    Added nlp DB schema.
    (cherry picked from commit 6dc89a164f9b9503c0df7e0fafc515a6d7ff41a7)

--- services/pgsamples/init_db.sh
+++ services/pgsamples/init_db.sh
@@ -10,6 +10,9 @@ DB_NAME="db_samples"
 DATA_DIR="/data"
 DB_DUMP_FILE="db_samples.sql.gz"
 
+ANNOTATIONS_NLP_DB_SCHEMA_FILE="annotations_nlp_create_schema.sql"
+
+
 # create the user, the database and set up the access
 #
 echo "Creating database: $DB_NAME and user: $DB_USER"
@@ -17,6 +20,11 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
 CREATE DATABASE $DB_NAME;
 GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
+
+# create schemas
+#
+echo "Defining ANNOTATION DB schemas"
+psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME -f $DATA_DIR/$ANNOTATIONS_NLP_DB_SCHEMA_FILE
 
 echo "Restoring DB from dump"
 gunzip -c $DATA_DIR/$DB_DUMP_FILE | psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME
