8247f8ada0d013ca3e55bbd938ec9b868b72e000
kawsarnoor <kawsar.noor.15@ucl.ac.uk>
2022-01-31 23:29:45 +0000

    cogstackhub services now reading from Dockerhub

--- services/cogstackhub/client/node_modules/buefy/dist/cjs/chunk-f1df1c63.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Polyfills for SSR
-var isSSR = typeof window === 'undefined';
-var HTMLElement = isSSR ? Object : window.HTMLElement;
-var File = isSSR ? Object : window.File;
-
-exports.File = File;
-exports.HTMLElement = HTMLElement;
