85ebd6802e8ebb000043667626ffb21d3c8f49c5
vladd-bit <vlad.a.dinu@gmail.com>
2022-02-16 06:37:58 -0800

    Updated SSL security certificate generation for ES/OpenSearch/Nifi.

---
+++ security/create_es_client_cert.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+################################################################
+# 
+# This script creates client keys and certificates that can 
+#  be used by client's applications
+#
+
+set -e
+
+ES_CERTIFICATES_FOLDER="./es_certificates/"
+
+CERTIFICATE_TIME_VAILIDITY_IN_DAYS=730
+
+CLIENT_CERT_NAME="es_kibana_client"
+
+CA_ROOT_CERT="root-ca.pem"
+CA_ROOT_KEY="root-ca.key"
+
+if [ ! -e $CA_ROOT_CERT ]; then
+	echo "Root CA certificate and key does not exist: $CA_ROOT_CERT , $CA_ROOT_KEY"
+	exit 1
+fi
+
+SUBJ_LINE="/C=UK/ST=UK/L=UK/O=cogstack/OU=cogstack/CN=CLIENT"
+
+echo "Generating a key for: $CLIENT_CERT_NAME"
+openssl genrsa -out "$CLIENT_CERT_NAME-pkcs12.key" 4096
+
+echo "Converting the key to PKCS 12"
+openssl pkcs8 -v1 "PBE-SHA1-3DES" -in "$CLIENT_CERT_NAME-pkcs12.key" -topk8 -out "$CLIENT_CERT_NAME.key" -nocrypt
+
+echo "Generating the certificate ..."
+openssl req -new -key "$CLIENT_CERT_NAME.key" -out "$CLIENT_CERT_NAME.csr" -subj $SUBJ_LINE
+
+# -config <(cat /etc/ssl/openssl.cnf <(printf "\nsubjectAltName=DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost"))
+
+echo "Signing the certificate ..."
+openssl x509 -req -days $CERTIFICATE_TIME_VAILIDITY_IN_DAYS -in "$CLIENT_CERT_NAME.csr" -CA $CA_ROOT_CERT -CAkey $CA_ROOT_KEY -CAcreateserial -out "$CLIENT_CERT_NAME.pem"
+
+#-extfile <(printf "\nsubjectAltName=DNS:esnode-1,DNS:esnode-2,DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost") 
+
+mv "$CLIENT_CERT_NAME-pkcs12.key" $ES_CERTIFICATES_FOLDER
+mv "$CLIENT_CERT_NAME.key" $ES_CERTIFICATES_FOLDER
+mv "$CLIENT_CERT_NAME.csr" $ES_CERTIFICATES_FOLDER
+mv "$CLIENT_CERT_NAME.pem" $ES_CERTIFICATES_FOLDER
--- security/create_es_node_cert.sh
+++ security/create_es_node_cert.sh
@@ -25,7 +25,9 @@ if [ ! -e $CA_ROOT_CERT ]; then
 	exit 1
 fi
 
-SUBJ_LINE="/C=UK/ST=UK/L=UK/O=cogstack/OU=cogstack/CN=$1/emailAddress=admin@cogstack.net"
+# The SUBJECT LINE is important, the CN (Company Name) should be the docker service container name, this is used for host VERIFICATION afterwards (see kibana/config/kibana_*.yml)
+
+SUBJ_LINE="/C=UK/ST=UK/L=UK/O=cogstack/OU=cogstack/CN=$1"
 
 echo "Generating a key for: $1"
 openssl genrsa -out "$1-pkcs12.key" 4096
@@ -34,10 +36,14 @@ echo "Converting the key to PKCS 12"
 openssl pkcs8 -v1 "PBE-SHA1-3DES" -in "$1-pkcs12.key" -topk8 -out "$1.key" -nocrypt
 
 echo "Generating the certificate ..."
-openssl req -new -key "$1.key" -out "$1.csr" -subj $SUBJ_LINE -config <(cat /etc/ssl/openssl.cnf <(printf "\nsubjectAltName=DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost"))
+openssl req -new -key "$1.key" -out "$1.csr" -subj $SUBJ_LINE
+
+# -config <(cat /etc/ssl/openssl.cnf <(printf "\nsubjectAltName=DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost"))
 
 echo "Signing the certificate ..."
-openssl x509 -req -extfile <(printf "\nsubjectAltName=DNS:esnode-1,DNS:esnode-2,DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost") -days $CERTIFICATE_TIME_VAILIDITY_IN_DAYS -in "$1.csr" -CA $CA_ROOT_CERT -CAkey $CA_ROOT_KEY -CAcreateserial -out "$1.pem"
+openssl x509 -req -days $CERTIFICATE_TIME_VAILIDITY_IN_DAYS -in "$1.csr" -CA $CA_ROOT_CERT -CAkey $CA_ROOT_KEY -CAcreateserial -out "$1.pem"
+
+#-extfile <(printf "\nsubjectAltName=DNS:esnode-1,DNS:esnode-2,DNS:elasticsearch-1,DNS:elasticsearch-2,DNS:elasticsearch-node-1,DNS:elasticsearch-node-2,DNS:elasticsearch-cogstack-node-2,DNS:elasticsearch-cogstack-node-1,DNS:localhost") 
 
 mv "$1-pkcs12.key" $ES_CERTIFICATES_FOLDER
 mv "$1.key" $ES_CERTIFICATES_FOLDER
