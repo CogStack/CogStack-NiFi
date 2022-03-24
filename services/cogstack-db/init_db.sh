467641c097f8343719c9f3eb2ca038defb44602c
vladd-bit <vlad.a.dinu@gmail.com>
2022-03-24 11:21:04 +0000

    Extra settings to cogstack-db.(Default databank db name)

--- services/cogstack-db/init_db.sh
+++ services/cogstack-db/init_db.sh
@@ -5,7 +5,7 @@ set -e
 
 # global variables
 #
-DB_NAME="databank"
+POSTGRES_DATABANK_DB="databank"
 DATA_DIR="/data"
 ANNOTATIONS_NLP_DB_SCHEMA_FILE="annotations_nlp_create_schema.sql"
 
@@ -14,16 +14,16 @@ DB_SCHEMA_PREFIX="cogstack_db"
 
 # create the user, the database and set up the access
 #
-echo "Creating database: $DB_NAME and user: $POSTGRES_USER"
+echo "Creating database: $POSTGRES_DATABANK_DB and user: $POSTGRES_USER"
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
-CREATE DATABASE $DB_NAME;
-GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $POSTGRES_USER;
+CREATE DATABASE $POSTGRES_DATABANK_DB;
+GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DATABANK_DB TO $POSTGRES_USER;
 EOSQL
 
 # create schemas
 #
 echo "Defining ANNOTATION DB schemas"
-psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME -f $DATA_DIR/$ANNOTATIONS_NLP_DB_SCHEMA_FILE
+psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DATABANK_DB -f $DATA_DIR/$ANNOTATIONS_NLP_DB_SCHEMA_FILE
 
 # custom db schema
 echo "Creating custom databank DB schemas"
@@ -31,7 +31,7 @@ echo "Creating custom databank DB schemas"
 file_paths=$(find $DATA_DIR/ -name "$DB_SCHEMA_PREFIX*")
 
 for file_path in $file_paths; do
-    psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME -f $file_path;
+    psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DATABANK_DB -f $file_path;
 done
 
 # cleanup
