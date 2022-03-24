dfe0cbfd884e1aecb50a06d4b42839dac5c1df46
vladd-bit <vlad.a.dinu@gmail.com>
2022-03-24 11:01:26 +0000

    Added production db init script and setup.

--- services/cogstack-db/init_db.sh
+++ services/cogstack-db/init_db.sh
@@ -9,6 +9,9 @@ DB_NAME="databank"
 DATA_DIR="/data"
 ANNOTATIONS_NLP_DB_SCHEMA_FILE="annotations_nlp_create_schema.sql"
 
+# database schemas that you wish to be created
+DB_SCHEMA_PREFIX="cogstack_db"
+
 # create the user, the database and set up the access
 #
 echo "Creating database: $DB_NAME and user: $POSTGRES_USER"
@@ -21,6 +24,15 @@ EOSQL
 #
 echo "Defining ANNOTATION DB schemas"
 psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME -f $DATA_DIR/$ANNOTATIONS_NLP_DB_SCHEMA_FILE
+
+# custom db schema
+echo "Creating custom databank DB schemas"
+
+file_paths=$(find $DATA_DIR/ -name "$DB_SCHEMA_PREFIX*")
+
+for file_path in $file_paths; do
+    psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME -f $file_path;
+done
 
 # cleanup
 #
---
+++ services/cogstack-db/schemas/cogstack_db_databank.sql
@@ -0,0 +1,23 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
