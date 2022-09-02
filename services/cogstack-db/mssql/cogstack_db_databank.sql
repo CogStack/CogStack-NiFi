73babd2abcb37b70bd0e0511ed0ed10fad904fc7
vladd-bit <vlad.a.dinu@gmail.com>
2022-09-02 15:14:35 +0100

    Corrected mssql example.

--- services/cogstack-db/mssql/cogstack_db_databank.sql
+++
@@ -1,4 +0,0 @@
-CREATE SCHEMA [CogStack]
-GO
-
-    
--- services/cogstack-db/mssql/init_db.sh
+++ services/cogstack-db/mssql/init_db.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
 
-set -e
 
+# this script is a hack to work around the fact that MSSQL images don't have a predefined startup script point like PGSQL
+# https://github.com/microsoft/mssql-docker/issues/2
+
+# start up mssql-server
+
+process_id=$(pgrep sqlservr)
+
+if [[ -z ${process_id} ]]; then
+    echo "Starting mssql-server..."
+    /opt/mssql/bin/sqlservr &
+    sleep 20s
+else
+    echo "mssql-server already started, PID:"$process_id
+fi
+
+# global variables
+#
+DATABANK_DB="databank"
+DATA_DIR="/data"
+ANNOTATIONS_NLP_DB_SCHEMA_FILE="annotations_nlp_create_schema.sql"
+
+# database schemas that you wish to be created
+DB_SCHEMA_PREFIX="cogstack_db"
+
+# create the user, the database and set up the access
+#
+echo "Creating database: $DATABANK_DB and user: $MSSQL_SA_USER"
+/opt/mssql-tools/bin/sqlcmd -S "localhost,1433"  -U $MSSQL_SA_USER -P $MSSQL_SA_PASSWORD -Q "
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '$DATABANK_DB')
+BEGIN
+    CREATE DATABASE [$DATABANK_DB]
+END
+GO
+    USE [$DATABANK_DB]
+"
+
+# custom db schema
+echo "Creating custom databank DB schemas"
+
+file_paths=$(find $DATA_DIR/ -name "$DB_SCHEMA_PREFIX*")
+
+for file_path in $file_paths; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U $MSSQL_SA_USER -P $MSSQL_SA_PASSWORD -d $DATABANK_DB -i $file_path;
+done
+
+while :; do
+    sleep 10
+done
