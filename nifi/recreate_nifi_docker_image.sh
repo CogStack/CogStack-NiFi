3973bd6c80add36c6319da28c4142b5055590d8b
vladd-bit <vlad.a.dinu@gmail.com>
2022-12-02 13:34:06 +0000

    NiFi: added gid/uid setting to img re-build script.

--- nifi/recreate_nifi_docker_image.sh
+++ nifi/recreate_nifi_docker_image.sh
@@ -5,4 +5,12 @@ set -e
 NIFI_GID=${NIFI_GID:-1000}
 NIFI_UID=${NIFI_UID:-1000}
 
+if [[ $NIFI_UID == 1000 ]]; then
+    NIFI_UID=$(id -u)
+fi
+
+if [[ $NIFI_GID == 1000 ]]; then
+    NIFI_GID=$(id -g)
+fi
+
 docker build  --build-arg GID=${NIFI_GID} --build-arg UID=${NIFI_UID} -t cogstacksystems/cogstack-nifi:latest -f Dockerfile .
