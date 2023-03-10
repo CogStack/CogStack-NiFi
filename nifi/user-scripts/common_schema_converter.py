1d1a763d15dc20d90ea5e3e5d65b96683ffa7d91
vladd-bit <vlad.a.dinu@gmai.com>
2023-03-10 00:58:12 +0000

    NiFi: common schema updates.

--- nifi/user-scripts/common_schema_converter.py
+++ nifi/user-scripts/common_schema_converter.py
@@ -1,7 +1,5 @@
 import traceback
 import io
-import sys
-import os
 import json
 import avro.schema
 
@@ -35,15 +33,14 @@ class PyStreamCallback(StreamCallback):
         bytes_arr = IOUtils.toByteArray(inputStream)
         bytes_io = io.BytesIO(bytes_arr)
 
-        json_data_records = json.loads(str(bytes_io.read()).encode("UTF-8"))
+        json_data_records = json.loads(bytes_io.read())
 
-        type_mapping = {"long": "int", "int": "int", "float" : "float", "string": "str", "boolean": "bool", "double": "float", "byte": "bytes"}
-
+     
         available_mapping_keys = {}
         for k,v in json_mapper_schema.iteritems():
             if v is not "":
                 available_mapping_keys[k] = v
-
+      
         new_json = []
         for _record in json_data_records:
             record = {}
@@ -53,64 +50,7 @@ class PyStreamCallback(StreamCallback):
             new_json.append(record)
 
         outputStream.write(json.dumps(new_json).encode("UTF-8"))
-
-        """
-        with DataFileWriter(outputStream, DatumWriter(), avro_cogstack_schema) as writer, \
-            DataFileReader(bytes_io, DatumReader()) as reader:
-
-            schema_fields = avro_cogstack_schema.props["fields"]
-            dict_fields_types = {}
-
-            for field in schema_fields:
-                dict_fields_types[field.name] = "string"
-                tmp_list = json.loads(str(field.type))
-                
-                if len(tmp_list) > 1 and type(tmp_list) is not str:
-                    if type(tmp_list[1]) is dict:
-                        dict_fields_types[field.name] = tmp_list[1]["type"]
-                    elif type(tmp_list[0]) is dict:
-                        dict_fields_types[field.name] = tmp_list[0]["type"]
-                    elif str(tmp_list[1]) != "null":
-                        dict_fields_types[field.name] = tmp_list[1]
-                    elif str(tmp_list[0]) != "null":
-                        dict_fields_types[field.name] = tmp_list[0]
-                else:
-                    dict_fields_types[field.name] = field.type
-                
-            available_mapping_keys = {}
-            for k,v in json_mapper_schema.iteritems():
-                if v is not "":
-                    available_mapping_keys[k] = v
-            
-            test_records = [{ "docid" : "1",
-            "sampleid" : 1,
-            "dct" : "sf",
-            "binarydoc": bytes("test")}]
-
-            bytes_writer = io.BytesIO()
-            encoder = avro.io.BinaryEncoder(bytes_writer)
-
-            test_schema = avro.schema.parse(reader.schema, validate_enum_symbols=False)
-            test_writer = DatumWriter(avro_cogstack_schema)
-            #test_writer.write_data(avro_cogstack_schema, test_records[0], encoder)
-
-            # re-map the value to the new keys
-            for _record in reader:
-                record = {}
-                
-                for k, v in available_mapping_keys.iteritems():
-                    if v in _record.keys():
-                        record[k] = str(_record[v]).encode("utf-8") #getattr(__builtins__, type_mapping[dict_fields_types[k]])(_record[v])
-                        #log.info(str(type(getattr(__builtins__, type_mapping[dict_fields_types[k]]))))
-                        #_record[v] #
-                #for k in json_mapper_schema.keys():
-                #    if k not in record.keys():
-                #        record[k] = None
-                #writer.append({"document_Fields_text": "1", "document_Id": "1"})
-                log.info(str(record))
-                test_writer.write_data(avro_cogstack_schema, record, encoder)
-                log.info("DONE")
-        """
+       
 
 if flowFile != None:
     JSON_MAPPER_SCHEMA_LOCATION_PROPERTY_NAME="mapper_schema_location"
