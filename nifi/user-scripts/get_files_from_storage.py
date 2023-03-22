cfb608c39e62eda494417e09d2bcb71eee22f7e1
vladd-bit <vlad.a.dinu@gmail.com>
2023-03-22 11:12:36 +0000

    NiFi: scripts & template update.

--- nifi/user-scripts/get_files_from_storage.py
+++ nifi/user-scripts/get_files_from_storage.py
@@ -9,26 +9,34 @@ import numpy
 import base64
 import sys
 
-# This is the DATA directory inside the postgres database Docker image, or it could be be a folder on the local system
-root_project_data_dir="/opt/data/ingestion/"
-folder_to_ingest="2022"
+# get the arguments from the "Command Arguments" property in NiFi, we are looking at anything after the 1st arg (which is the script name)
+# example args: ['/opt/nifi/user-scripts/get_files_from_storage.py', 'folder_pattern=.*\\d{4}\\/\\d{2}\\/\\d{2}', 'folder_to_ingest=2022', 'file_id_csv_column_name_match=file_name_id_no_ext']
 
-processed_folder_dump="processed_" + folder_to_ingest
-file_name_pattern="*"
-
-file_ext_to_match=".txt"
+folder_to_ingest = "2022"
+folder_pattern = ".*\d{4}\/\d{2}\/\d{2}"
+file_id_csv_column_name_match="file_name_id_no_ext"
 
 encoding="UTF-8"
 
-processed_folder_dump_path = os.path.join("/opt/nifi/user-scripts/logs/", processed_folder_dump)
+for arg in sys.argv:
+    _arg = arg.split("=", 1)
+    if _arg[0] == "folder_pattern":
+        folder_pattern = _arg[1] 
+    elif _arg[0] == "folder_to_ingest":
+        folder_to_ingest = _arg[1]
+    elif _arg[0] == "file_id_csv_column_name_match":
+        file_id_csv_column_name_match = _arg[1]
+
+# This is the DATA directory inside the postgres database Docker image, or it could be a folder on the local system
+root_project_data_dir="/opt/data/ingestion/"
+processed_folder_dump="processed_" + folder_to_ingest
+processed_folder_dump_path = os.path.join(os.environ.get("USER_SCRIPT_LOGS_DIR"), processed_folder_dump)
 
 # log file name
-ingested_folders_file = processed_folder_dump_path + ".txt"
+ingested_folders_file = processed_folder_dump_path + ".log"
 
-folder_pattern = ".*\d{4}\/\d{2}\/\d{2}"
 pattern_c = re.compile(folder_pattern)
 
-file_id_csv_column_name_match="file_name_id_no_ext"
 
 folders_ingested = {}
 
@@ -41,7 +49,7 @@ if os.path.exists(ingested_folders_file):
 output_data = []
 
 def get_files_and_metadata():
-    """_summary_
+    '''_summary_
         This will read the pdf documents and metadata.csv file from one folder and output them as a json,
         NiFi will handle the conversion of this json to a proper flowfile using the ConvertRecord processor.
 
@@ -56,7 +64,7 @@ def get_files_and_metadata():
                         ├── aaaabbbbccccdddd.pdf <- file is the ID
                         └── metadata.csv <- contains file ID for matching
 
-    """
+    '''
     for root, sub_directories, files in os.walk(os.path.join(root_project_data_dir, folder_to_ingest)):
         
         # it will only ingest
