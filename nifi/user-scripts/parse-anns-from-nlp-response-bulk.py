ccc03d0f2d3b550a9a397ad2ef4c1a25c3ebd250
vladd-bit <vlad.a.dinu@gmail.com>
2023-05-12 12:08:16 +0100

    NiFi: updated annotation scripts.

--- nifi/user-scripts/parse-anns-from-nlp-response-bulk.py
+++ nifi/user-scripts/parse-anns-from-nlp-response-bulk.py
@@ -45,11 +45,13 @@ class PyStreamCallback(StreamCallback):
 
         json_data_records = json.loads(bytes_io.read())
 
+        bytes_io.close()
+
         result = json_data_records["result"]
         medcat_info = json_data_records["medcat_info"]
         
         for annotated_text_record in result:
-            annotations = annotated_text_record["annotations"]
+            annotations = annotated_text_record["annotations"][0]
             footer = annotated_text_record["footer"]
 
             new_footer = {}
@@ -60,55 +62,49 @@ class PyStreamCallback(StreamCallback):
             for k,v in footer.iteritems():
                 if k in ORIGINAL_FIELDS_TO_INCLUDE:
                     new_footer[FIELD_META_PREFIX + k] = v
-
-            for annotation in annotations:
-                new_ann_record = {}
-                annotation_data = annotation.values()
-
-                # sometimes there's an empty annotation list
-                if len(annotation_data) == 0:
-                    log.info("Empty annotation list - " + footer[DOCUMENT_ID_FIELD_NAME])
-                    continue
-                # should now be a dictionary in a list
-                annotation_data = annotation_data[0]
+            
+            # sometimes there's an empty annotation list
+            if len(annotations.keys()) == 0:
+                log.info("Empty annotation list - " + footer[DOCUMENT_ID_FIELD_NAME])
+                continue
+            
+            anns_ids=[]
+            for ann_id, annotation in annotations.iteritems():
                 ignore_annotation = False
+                anns_ids.append(ann_id)
+
                 for type_to_ignore in ANNOTATION_TYPES_TO_IGNORE:
-                    if type_to_ignore in annotation_data["types"]:
-                        log.info("====================")
+                    if type_to_ignore in annotation["types"]:
                         ignore_annotation = True
                         break
                 
                 if ignore_annotation is False:
-                    for k,v in annotation_data.iteritems():
-                        new_ann_record[FIELD_NLP_PREFIX + str(k)] = v
-
-                        # create the new _id for the annotation record in ElasticSearch
-                        
-                        new_ann_record["timestamp"] = annotated_text_record["timestamp"]
-                        new_ann_record["service_model"] = medcat_info["service_model"]
-                        new_ann_record["service_version"] = medcat_info["service_version"]
-
-                        new_ann_record[FIELD_META_PREFIX + DOCUMENT_ID_FIELD_NAME] = doc_id
-                        new_ann_record.update(new_footer)
-                        
-                        document_annotation_id = str(doc_id) + "_" + str(annotation_data[ANNOTATION_ID_FIELD_NAME])
-
-                        new_flow_file = session.create(flowFile)
-                        new_flow_file = session.putAttribute(new_flow_file, "document_annotation_id", document_annotation_id)
-                        
-                        new_flow_file = session.write(new_flow_file, WriteContentCallback(json.dumps(new_ann_record).encode("UTF-8")))
-                        flowFiles.append(new_flow_file)
-
+                    new_ann_record = {}
+                    new_ann_record[FIELD_NLP_PREFIX + str(ann_id)] = annotation
+ 
+                    # create the new _id for the annotation record in ElasticSearch
+                    new_ann_record["timestamp"] = annotated_text_record["timestamp"]
+                    new_ann_record["service_model"] = medcat_info["service_model"]
+                    new_ann_record["service_version"] = medcat_info["service_version"]
+ 
+                    new_ann_record[FIELD_META_PREFIX + DOCUMENT_ID_FIELD_NAME] = doc_id
+                    new_ann_record.update(new_footer)
+                    
+                    document_annotation_id = str(doc_id) + "_" + str(annotation[ANNOTATION_ID_FIELD_NAME])
+ 
+                    new_flow_file = session.create(flowFile)
+                    new_flow_file = session.putAttribute(new_flow_file, "document_annotation_id", document_annotation_id)
+                    new_flow_file = session.write(new_flow_file, WriteContentCallback(json.dumps(new_ann_record).encode("UTF-8")))
+                    flowFiles.append(new_flow_file)
 
 if flowFile != None:
     DOCUMENT_ID_FIELD_NAME = str(context.getProperty("document_id_field"))
     ANNOTATION_ID_FIELD_NAME = str(context.getProperty("annotation_id_field"))
 
     _tmp_ann_type_ignore = str(context.getProperty("ignore_annotation_types"))
-
     _tmp_original_record_fields_to_include = str(context.getProperty("original_record_fields_to_include"))
-    ORIGINAL_FIELDS_TO_INCLUDE = _tmp_ann_type_ignore.split(",") if _tmp_ann_type_ignore.lower() != "none" else []
 
+    ORIGINAL_FIELDS_TO_INCLUDE = _tmp_original_record_fields_to_include.split(",") if _tmp_original_record_fields_to_include.lower() != "none" else []
     ANNOTATION_TYPES_TO_IGNORE = _tmp_ann_type_ignore.split(",") if _tmp_ann_type_ignore.lower() != "none" else []
 
     try:
