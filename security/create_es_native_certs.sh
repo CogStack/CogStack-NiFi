567af65f361f6e7f7dd679ea267e2c3a3ada1f79
vladd-bit <vlad.a.dinu@gmail.com>
2023-06-30 10:26:46 +0100

    Security: fixed cert generation.

--- security/create_es_native_certs.sh
+++ security/create_es_native_certs.sh
@@ -12,14 +12,19 @@ set -e
 # spin up the container that creates the certs 
 
 if [ ! -d "es_certificates/es_native/elasticsearch" ]; then
+
+    echo "Removing previous cert container and volume if existent...."
+    docker container rm -f $(docker ps -a -q --filter name="deploy_es_native_create_certs_run_*") || true
+    docker volume rm $(docker volume ls --filter name=deploy_elasticsearch-certs- -q) -f || true
+
     echo "Certificates for es_native not present, creating them now ..."
     docker-compose -f ../deploy/services.yml run es_native_create_certs
 
     echo "Removing cert container and volume...."
     docker container rm -f $(docker ps -a -q --filter name="deploy_es_native_create_certs_run_*")
-    docker volume rm elasticsearch-certs-vol -f
+    docker volume rm $(docker volume ls --filter name=deploy_elasticsearch-certs- -q) -f
 else
     echo "Certificates found, skipping creating, if you want to recreate delete the ./es_certificates/es_native folder"
 fi
 
-chown -R $USER:$USER ./es_certificates/elasticsearch
+chown -R $USER ./es_certificates/elasticsearch
