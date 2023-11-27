84a18f3136e172c699cb5993879d8a2d8a535921
vladd-bit <vlad.a.dinu@gmail.com>
2023-11-27 11:15:02 +0000

    NiFi scripts: anon doc script improvement (allow modelpack path).

--- nifi/user-scripts/anonymise_doc.py
+++ nifi/user-scripts/anonymise_doc.py
@@ -1,9 +1,14 @@
 from medcat.utils.ner.deid import DeIdModel
 import sys
-
+import os
 input_text = sys.stdin.read()
 
-model_pack_path = "/opt/models/de_id_base.zip"
+model_pack_path = os.environ.get("MODEL_PACK_PATH", "/opt/models/de_id_base.zip")
+
+for arg in sys.argv:
+    _arg = arg.split("=", 1)
+    if _arg[0] == "model_pack_path":
+        model_pack_path = _arg[1]
 
 cat = DeIdModel.load_model_pack(model_pack_path)
 
