aef3de0e1f400706b95cae4d1a4de0206536f8a9
vladd-bit <vlad.a.dinu@gmail.com>
2024-01-25 16:16:02 +0000

    NiFi Scripts: updated annotation manager.

--- nifi/user-scripts/annotation_manager.py
+++ nifi/user-scripts/annotation_manager.py
@@ -5,6 +5,7 @@ import sys
 from utils.sqlite_query import connect_and_query,check_db_exists,create_db_from_file
 
 global DOCUMENT_ID_FIELD_NAME
+global DOCUMENT_TEXT_FIELD_NAME
 global USER_SCRIPT_DB_DIR
 global DB_FILE_NAME
 global LOG_FILE_NAME
@@ -28,6 +29,8 @@ for arg in sys.argv:
         INDEX_DB_FILE_NAME = _arg[1] 
     elif _arg[0] == "document_id_field":
         DOCUMENT_ID_FIELD_NAME = _arg[1]
+    elif _arg[0] == "document_text_field":
+        DOCUMENT_TEXT_FIELD_NAME = _arg[1]
     elif _arg[0] == "user_script_db_dir":
         USER_SCRIPT_DB_DIR = _arg[1]
     elif _arg[0] == "log_file_name":
@@ -37,8 +40,6 @@ for arg in sys.argv:
 
 global output_stream
 
-output_stream = {}
-
 def main():
     input_stream = sys.stdin.read()
 
@@ -46,26 +47,38 @@ def main():
         log_file_path = os.path.join(USER_SCRIPT_LOGS_DIR, str(LOG_FILE_NAME))
         db_file_path = os.path.join(USER_SCRIPT_DB_DIR, INDEX_DB_FILE_NAME)
 
-        json_data_record = json.loads(input_stream)
+        json_data_records = json.loads(input_stream)
 
         if len(check_db_exists("annotations", db_file_path)) == 0:
             create_db_from_file(ANNOTATION_DB_SQL_FILE_PATH, db_file_path)
 
-        if OPERATION_MODE == "check":
-            document_id = str(json_data_record["id"])
-            query = "SELECT * FROM annotations WHERE elasticsearch_id LIKE (" + document_id + ")"
-            result = connect_and_query(query, db_file_path)
-            
-            output_stream = input_stream
-            if len(result) > 1:
-                output_stream = len(result)
-            else:
-                output_stream = json_data_record
+        output_stream = {}
 
-        elif OPERATION_MODE == "insert":
-            query = "INSERT INTO annotations (elasticsearch_id) VALUES (" + '"' + str(json_data_record["meta.docid"]) + "_" + str(json_data_record["nlp.id"]) + '"' +")"
-            result = connect_and_query(query, db_file_path, sql_script_mode=True)
-            output_stream = json_data_record
+        # keep original structure of JSON:
+        output_stream["content"] = []
+
+        records = json_data_records
+        if "content" in json_data_records.keys():
+            records = json_data_records["content"]
+            
+        # if we are parsing an annotation only (post-NLP)
+        if type(records) == dict:
+            records = [records]
+            output_stream = []
+        
+        for record in records:
+            if OPERATION_MODE == "check":
+                document_id = str(record[DOCUMENT_ID_FIELD_NAME])
+                query = "SELECT * FROM annotations WHERE elasticsearch_id LIKE (%" + document_id + "_%)"
+                result = connect_and_query(query, db_file_path)
+
+                if len(result) < 1:
+                    output_stream["content"].append(record)
+
+            elif OPERATION_MODE == "insert":
+                query = "INSERT INTO annotations (elasticsearch_id) VALUES (" + '"' + str(record["meta.docid"]) + "_" + str(record["nlp.id"]) + '"' +")"
+                result = connect_and_query(query, db_file_path, sql_script_mode=True)
+                output_stream = record
 
     except Exception as exception:
         if os.path.exists(log_file_path):
