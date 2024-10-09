425a43db6d0aec60e7ce77b3dc98c04ab04f139f
vladd-bit <vlad.a.dinu@gmail.com>
2024-10-10 00:21:32 +0100

    NiFi: updating to 2.0.0-M4 part I

--- security/nifi_toolkit_security.sh
+++ security/nifi_toolkit_security.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-NIFI_TOOLKIT_VERSION=${NIFI_VERSION:-"1.25.0"}
+NIFI_TOOLKIT_VERSION=${NIFI_VERSION:-"2.0.0-M4"}
 
 if [[ -z "${NIFI_TOOLKIT_VERSION}" ]]; then
     NIFI_TOOLKIT_VERSION=$NIFI_TOOLKIT_VERSION
@@ -38,14 +38,14 @@ PATH_TO_NIFI_PROPERTIES_FILE="./../nifi/conf/nifi.properties"
 KEY_SIZE=4096
 
 # -n, --hostnames <arg> Comma separated list of hostnames i.e "server1,server2,localhost" etc.
-HOSTNAMES="localhost"
+HOSTNAMES="nifi"
 
 OUTPUT_DIRECTORY="./nifi_certificates"
 
 # -C,--clientCertDn <arg> Generate client certificate suitable for use in browser with specified DN (Can be specified multiple times)
 # this should respect whatever is used to generate the other certificate with regards CN=nifi, this needs to match the HOSTNAME of the nifi container(s)
 if [[ -z "${NIFI_SUBJ_LINE_CERTIFICATE_CN}" ]]; then
-    NIFI_SUBJ_LINE_CERTIFICATE_CN="CN=cogstack, OU=cogstack, C=UK, ST=UK, L=UK, O=cogstack"
+    NIFI_SUBJ_LINE_CERTIFICATE_CN="CN=cogstack , OU=cogstack, C=UK, ST=UK, L=UK, O=cogstack"
     echo "NIFI_SUBJ_LINE_CERTIFICATE_CN not set, defaulting to NIFI_SUBJ_LINE_CERTIFICATE_CN=CN=cogstack, OU=cogstack, C=UK, ST=UK, L=UK, O=cogstack"
 else
     NIFI_SUBJ_LINE_CERTIFICATE_CN=${NIFI_SUBJ_LINE_CERTIFICATE_CN}
