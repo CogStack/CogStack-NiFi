64235b11aedeefbde887a30f569060061ba80183
vladd-bit <vlad.a.dinu@gmail.com>
2025-07-08 00:56:29 +0100

    NiFi: scripting cleanup + added new processors for avro binary doc field conversion.

---
+++ nifi/user-python-extensions/convert_avro_binary_field_to_base64.py
@@ -0,0 +1,115 @@
+import io
+import base64
+import traceback
+from logging import Logger
+
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+from avro.schema import Schema
+
+from nifiapi.flowfiletransform import FlowFileTransform, FlowFileTransformResult
+from nifiapi.properties import ProcessContext, PropertyDescriptor
+from py4j.java_gateway import JVMView, JavaObject
+
+
+class ConvertAvroBinaryRecordFieldToBase64(FlowFileTransform):
+    identifier = None
+    logger: Logger = Logger(__qualname__)
+
+
+    class Java:
+        implements = ['org.apache.nifi.python.processor.FlowFileTransform']
+
+    class ProcessorDetails:
+        version = '0.0.1'
+
+    def __init__(self, jvm: JVMView):
+        """
+        Args:
+            jvm (JVMView): Required, Store if you need to use Java classes later.
+        """
+        self.jvm = jvm
+
+        self.operation_mode = None
+        self.binary_field_name = None
+        self.record_id_field_name = None
+
+        # this is directly mirrored to the UI
+        self._properties = [
+            PropertyDescriptor(name="binary_field_name", description="Avro field containing binary data", default_value="not_set"),
+            PropertyDescriptor(name="operation_mode", description="Decoding mode (e.g. base64 or raw)", default_value="base64"),
+            PropertyDescriptor(name="record_id_field_name", description="Field name containing document ID", default_value="not_set"),
+        ]
+
+    def getPropertyDescriptors(self):
+        return self._properties
+
+    def set_logger(self, logger: Logger):
+        self.logger = logger
+
+    def set_properties(self, properties: dict):
+        """ Gets the properties from the processor's context and sets them as instance variables.
+
+        Args:
+            properties (dict): dictionary containing property names and values.
+        """
+
+        for k, v in list(properties.items()):
+            self.logger.debug(f"property set '{k.name}' with value '{v}'")
+            if hasattr(self, k.name):
+                setattr(self, k.name, v)
+
+    def transform(self, context: ProcessContext, flowFile: JavaObject) -> FlowFileTransformResult: # type: ignore
+        try:
+            self.process_context = context
+            self.set_properties(context.getProperties())
+
+            # read avro record
+            input_raw_bytes: bytearray = flowFile.getContentsAsBytes() # type: ignore
+            input_byte_buffer: io.BytesIO  = io.BytesIO(input_raw_bytes)
+            reader: DataFileReader = DataFileReader(input_byte_buffer, DatumReader())
+
+            schema: Schema | None = reader.datum_reader.writers_schema
+
+            # Write them to a binary avro stream
+            output_byte_buffer = io.BytesIO()
+            writer = DataFileWriter(output_byte_buffer, DatumWriter(), schema)
+
+            for record in reader:
+                if type(record) is dict:
+                    record_document_binary_data = record.get(str(self.binary_field_name), None)
+
+                    if record_document_binary_data is not None:
+                        if self.operation_mode == "base64":
+                            record_document_binary_data = base64.b64encode(record_document_binary_data).decode()
+                    else:
+                        self.logger.info("No binary data found in record, using empty content")
+                else:
+                    raise TypeError("Expected Avro record to be a dictionary, but got: " + str(type(record)))
+
+                _tmp_record = {}
+                _tmp_record[str(self.binary_field_name)] = record_document_binary_data
+
+                for k, v in record.items():
+                    if k != str(self.binary_field_name):
+                        _tmp_record[k] = v
+
+                writer.append(_tmp_record)
+
+            input_byte_buffer.close()
+            reader.close()
+
+            writer.flush()
+            writer.close()
+            output_byte_buffer.seek(0)
+
+            # add properties to flowfile attributes
+            attributes: dict = {k: str(v) for k, v in flowFile.getAttributes().items()} # type: ignore
+            attributes["record_id_field_name"] = str(self.record_id_field_name)
+            attributes["binary_field"] = str(self.binary_field_name)
+            attributes["operation_mode"] = str(self.operation_mode)
+
+            return FlowFileTransformResult(relationship="success", attributes=attributes, contents=output_byte_buffer.getvalue())
+        except Exception as exception:
+            self.logger.error("Exception during Avro processing: " + traceback.format_exc())
+            raise exception
---
+++ nifi/user-scripts/record_decompress_cerner_blob.py
@@ -0,0 +1,96 @@
+import sys
+import json
+import base64
+from utils.cerner_blob import DecompressLzwCernerBlob
+
+
+""" This script decompresses Cerner LZW compressed blobs from a JSON input stream.
+    It expects a JSON array of records, each containing a field with the binary data.
+    All RECORDS are expected to have the same fields, and presumably belonging to the same DOCUMENT.
+    All the fields of these records should have the same field values, except for the binary data field.
+    The binary data field is expected to be a base64 encoded string, which will be concatenated according to 
+    the blob_sequence_order_field_name field, preserving the order of the blobs and composing the whole document (supposedly).
+    The final base64 enncoded string will be decoded back to binary data, then decompressed using LZW algorithm.
+"""
+
+# This needs to be investigated, records might have different charsets,
+#   currently only tested with "iso-8859-1"
+#   other frequently used encodings: "utf-16le", "utf-16be"
+# In some cases you will need to figure this out yourself, depending on
+#   the data source
+INPUT_CHARSET = "iso-8859-1"
+
+# expected (optional)
+OUTPUT_CHARSET = "windows-1252"
+
+# possible values:
+#   - binary: output binary code
+#   - string: output string after decompression 
+OUTPUT_MODE = "binary"
+
+BINARY_FIELD_NAME = "binary_data"
+
+BINARY_FIELD_SOURCE_ENCODING = "base64"
+
+BLOB_SEQUENCE_ORDER_FIELD_NAME = "blob_sequence_num"
+
+for arg in sys.argv:
+    _arg = arg.split("=", 1)
+    if _arg[0] == "output_mode":
+        OUTPUT_MODE = _arg[1]
+    elif _arg[0] == "input_charset":
+        INPUT_CHARSET = _arg[1]
+    elif _arg[0] == "output_charset":
+        OUTPUT_CHARSET = _arg[1]
+    elif _arg[0] == "log_file_name":
+        LOG_FILE_NAME = _arg[1]
+    elif _arg[0] == "binary_field_name":
+        BINARY_FIELD_NAME = _arg[1]
+    elif _arg[0] == "binary_field_source_encoding":
+        BINARY_FIELD_SOURCE_ENCODING = _arg[1]
+    elif _arg[0] == "blob_sequence_order_field_name":
+        BLOB_SEQUENCE_ORDER_FIELD_NAME = _arg[1]
+
+records = json.loads(sys.stdin.read())
+
+if not isinstance(records, list):
+    records = [records]
+
+concatenated_blob_sequence_order = {}
+
+output_merged_record = {}
+
+for record in records:
+    if BLOB_SEQUENCE_ORDER_FIELD_NAME in record.keys():
+        concatenated_blob_sequence_order[int(record[BLOB_SEQUENCE_ORDER_FIELD_NAME])] = record[BLOB_SEQUENCE_ORDER_FIELD_NAME]
+
+
+# take fields from the first record, doesn't matter which one,
+# as they are expected to be the same except for the binary data field
+for k, v in records[0].items():
+    if k not in output_merged_record.keys() and k != BINARY_FIELD_NAME:
+        output_merged_record[k] = v
+
+del records
+
+concatenated_blob_sequence_order = sorted(concatenated_blob_sequence_order.items())
+
+output_merged_record[BINARY_FIELD_NAME] = ""
+for i in concatenated_blob_sequence_order:
+    output_merged_record[BINARY_FIELD_NAME] += i[1]
+
+del concatenated_blob_sequence_order
+
+output_merged_record[BINARY_FIELD_NAME] = base64.b64decode(output_merged_record[BINARY_FIELD_NAME])
+
+
+input_cerner_blob = str(output_merged_record[BINARY_FIELD_NAME], INPUT_CHARSET).encode(INPUT_CHARSET)
+
+decompress_blob = DecompressLzwCernerBlob()
+decompress_blob.decompress(input_cerner_blob) # type: ignore
+
+
+if OUTPUT_MODE == "binary":
+    sys.stdout.buffer.write(bytes(decompress_blob.output_stream))
+else:
+    sys.stdout.write(decompress_blob.output_stream.decode(OUTPUT_CHARSET))
