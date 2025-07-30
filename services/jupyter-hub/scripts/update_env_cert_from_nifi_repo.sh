44972d80e1c3bed3b6db1ae1db37ef7b45c2516d
vladd-bit <vlad.a.dinu@gmail.com>
2025-07-30 22:16:03 +0100

    Removed subtree.

--- services/jupyter-hub/scripts/update_env_cert_from_nifi_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/deploy/general.env > ./env/general.env
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/root-ca.key > ./security/root-ca.key
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/root-ca.pem > ./security/root-ca.pem
