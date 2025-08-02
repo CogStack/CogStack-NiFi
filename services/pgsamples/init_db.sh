c341bba8d7346a7604399d9d5015759f9da4a5b9
vladd-bit <vlad.a.dinu@gmail.com>
2025-08-02 23:00:55 +0100

    Services: updated pg samples (simplified db). Deploy: updated service defs for pgsql containers + gitea port exposure.

--- services/pgsamples/init_db.sh
+++ services/pgsamples/init_db.sh
@@ -10,26 +10,25 @@ DB_USER_PASSWORD=${POSTGRES_PASSWORD_SAMPLES:-"test"}
 DB_NAME="db_samples"
 
 DATA_DIR="/data"
-DB_DUMP_FILE="db_samples.sql.gz"
-
-ANNOTATIONS_NLP_DB_SCHEMA_FILE="annotations_nlp_create_schema.sql"
+DB_DUMP_FILE="/db_dump/samples_db_data.sql.gz"
+DB_SCHEMA_FILE="/schemas/samples_db_schema.sql"
 
 
 # create the user, the database and set up the access
 #
 echo "Creating database: $DB_NAME and user: $DB_USER"
 psql -v ON_ERROR_STOP=1 -U "$DB_USER" <<-EOSQL
-CREATE DATABASE $DB_NAME;
-CREATE USER $DB_USER WITH PASSWORD $DB_USER_PASSWORD;
+CREATE DATABASE $DB_NAME OWNER $DB_USER;
 GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 
 # create schemas
 #
-echo "Defining ANNOTATION DB schemas"
-psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME -f $DATA_DIR/$ANNOTATIONS_NLP_DB_SCHEMA_FILE
+echo "Defining DB schemas: $DB_SCHEMA_FILE"
+psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME -f $DATA_DIR/$DB_SCHEMA_FILE
 
-echo "Restoring DB from dump"
+
+echo "Restoring DB from dump: $DB_DUMP_FILE"
 gunzip -c $DATA_DIR/$DB_DUMP_FILE | psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME
 
 # cleanup
