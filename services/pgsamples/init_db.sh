b783653ea266cf428c9d53e63ddf382d324f9b73
vladd-bit <vlad.a.dinu@gmail.com>
2025-10-03 10:17:07 +0200

    Services: updated sameples-db script to include extra MIMIC-III tables.

--- services/pgsamples/init_db.sh
+++ services/pgsamples/init_db.sh
@@ -13,6 +13,15 @@ DATA_DIR="/data"
 DB_DUMP_FILE="/db_dump/samples_db_data.sql.gz"
 DB_SCHEMA_FILE="/schemas/samples_db_schema.sql"
 
+DB_DUMP_FILES_EXTRA_SQL_FILES=(
+    $DATA_DIR"/db_dump/encounters_pdf_text_small.sql"
+    $DATA_DIR"/db_dump/encounters_docx_small.sql"
+)
+
+DB_DUMP_FILES_EXTRA_SQL_GZ_FILES=(
+    $DATA_DIR"/db_dump/encounters_jpg_small.sql.gz"
+    $DATA_DIR"/db_dump/encounters_pdf_img_small.sql.gz"
+)
 
 # create the user, the database and set up the access
 #
@@ -27,9 +36,18 @@ EOSQL
 echo "Defining DB schemas: $DB_SCHEMA_FILE"
 psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME -f $DATA_DIR/$DB_SCHEMA_FILE
 
-
 echo "Restoring DB from dump: $DB_DUMP_FILE"
 gunzip -c $DATA_DIR/$DB_DUMP_FILE | psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME
+
+for sqlfile in "${DB_DUMP_FILES_EXTRA_SQL_FILES[@]}"; do
+  echo "=== Applying $sqlfile ==="
+  psql -v ON_ERROR_STOP=1 -U "$DB_USER" -d "$DB_NAME" -f "$sqlfile"
+done
+
+for gzfile in "${DB_DUMP_FILES_EXTRA_SQL_GZ_FILES[@]}"; do
+  echo "=== Applying $gzfile ==="
+gunzip -c "$gzfile" | psql -v ON_ERROR_STOP=1 -U $DB_USER -d $DB_NAME
+done
 
 # cleanup
 #
