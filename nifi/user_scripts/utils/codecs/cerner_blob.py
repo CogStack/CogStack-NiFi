9fcf75dcf8b178e53bb317a2ffb2cf8c1b609b44
vladd-bit <vlad.a.dinu@gmail.com>
2026-02-03 00:03:53 +0000

    NiFi scripts: lazy cerner blob decompression possible fix.

--- nifi/user_scripts/utils/codecs/cerner_blob.py
+++ nifi/user_scripts/utils/codecs/cerner_blob.py
@@ -9,7 +9,7 @@ class DecompressLzwCernerBlob:
     def __init__(self) -> None:
         self.MAX_CODES: int = 8192
         self.tmp_decompression_buffer: list[int] = [0] * self.MAX_CODES
-        self.lzw_lookup_table: list[LzwItem] = [LzwItem()] * self.MAX_CODES
+        self.lzw_lookup_table: list[LzwItem] = [LzwItem() for _ in range(self.MAX_CODES)]
         self.tmp_buffer_index: int = 0
         self.current_byte_buffer_index: int = 0
 
@@ -31,10 +31,11 @@ class DecompressLzwCernerBlob:
         for i in reversed(list(range(self.tmp_buffer_index + 1))):
             self.output_stream.append(self.tmp_decompression_buffer[i])
 
-    def decompress(self, input_stream: bytearray = bytearray()):
+    def decompress(self, input_stream: bytearray):
 
         byte_buffer_index: int = 0
-
+        self.output_stream = bytearray()
+        
         # used for bit shifts
         shift: int = 1
         current_shift: int = 1
@@ -45,7 +46,18 @@ class DecompressLzwCernerBlob:
 
         skip_flag: bool = False
 
+        def read_current_byte() -> int:
+            if byte_buffer_index >= len(input_stream):
+                raise ValueError("Truncated input_stream while decoding LZW")
+            return input_stream[byte_buffer_index]
+
+        if not input_stream:
+            return self.output_stream
+        if byte_buffer_index >= len(input_stream):
+            raise ValueError("Truncated input_stream while decoding LZW")
+        
         first_code = input_stream[byte_buffer_index]
+
 
         while True:
             if current_shift >= 9:
@@ -56,8 +68,7 @@ class DecompressLzwCernerBlob:
                     byte_buffer_index += 1
                     middle_code = input_stream[byte_buffer_index]
 
-                    first_code = (first_code << current_shift +
-                                  8) | (middle_code << current_shift)
+                    first_code = (first_code << (current_shift + 8)) | (middle_code << current_shift)
 
                     byte_buffer_index += 1
                     middle_code = input_stream[byte_buffer_index]
@@ -76,19 +87,19 @@ class DecompressLzwCernerBlob:
                 middle_code = input_stream[byte_buffer_index]
 
             if not skip_flag:
-                lookup_index = (first_code << current_shift) | (
-                    middle_code >> 8 - current_shift)
+                lookup_index = (first_code << current_shift) | (middle_code >> (8 - current_shift))
 
                 if lookup_index == 256:
                     shift = 1
                     current_shift += 1
-                    first_code = input_stream[byte_buffer_index]
+                    first_code = read_current_byte()
 
                     self.tmp_decompression_buffer = [0] * self.MAX_CODES
                     self.tmp_buffer_index = 0
 
-                    self.lzw_lookup_table = [LzwItem()] * self.MAX_CODES
+                    self.lzw_lookup_table = [LzwItem() for _ in range(self.MAX_CODES)]
                     self.code_count = 257
+                    previous_code = 0
                     continue
 
                 elif lookup_index == 257:  # EOF marker
@@ -107,10 +118,12 @@ class DecompressLzwCernerBlob:
                         self.tmp_decompression_buffer[self.tmp_buffer_index])
                     self.code_count += 1
             else:
-                self.lzw_lookup_table[self.code_count] = LzwItem(
-                    previous_code,
-                    self.tmp_decompression_buffer[self.tmp_buffer_index])
-                self.code_count += 1
+                if self.code_count < self.MAX_CODES:
+                    self.lzw_lookup_table[self.code_count] = LzwItem(
+                        previous_code,
+                        self.tmp_decompression_buffer[self.tmp_buffer_index]
+                    )
+                    self.code_count += 1
                 self.save_to_lookup_table(lookup_index)
             # end of skipit
 
